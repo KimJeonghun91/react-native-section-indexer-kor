4c96de7baa04f54e11b6c51ba9217e3ee9232e0f
devedu0714 <devedu@naver.com>
2020-07-24 18:39:42 +0900

    예제 수정

--- example/App.js
+++ example/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
 import SectionIndexer from 'react-native-section-indexer-kor';
 
 export default class App extends Component {
@@ -11,11 +11,41 @@ export default class App extends Component {
       { name: "김김김" },
       { name: "김김김" },
       { name: "김김김" },
+      { name: "김김김" },
+      { name: "김김김" },
+      { name: "김김김" },
+      { name: "김김김" },
+      { name: "김김김" },
+      { name: "김김김" },
+      { name: "김김김" },
+      { name: "김김김" },
+      { name: "김김김" },
+      { name: "김김김" },
+      { name: "김김김" },
+      { name: "김김김" },
       { name: "나나나" },
       { name: "나나나" },
       { name: "나나나" },
       { name: "나나나" },
       { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
+      { name: "나나나" },
       { name: "다다다" },
       { name: "다다다" },
       { name: "다다다" },
@@ -35,6 +65,47 @@ export default class App extends Component {
       { name: "아아아" },
       { name: "아아아" },
       { name: "아아아" },
+      { name: "아아아" },
+      { name: "아아아" },
+      { name: "아아아" },
+      { name: "아아아" },
+      { name: "아아아" },
+      { name: "아아아" },
+      { name: "아아아" },
+      { name: "아아아" },
+      { name: "아아아" },
+      { name: "아아아" },
+      { name: "아아아" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
+      { name: "타타타" },
       { name: "타타타" },
       { name: "타타타" },
       { name: "타타타" },
@@ -43,23 +114,44 @@ export default class App extends Component {
       { name: "타타타" },
       { name: "타타타" },
       { name: "하하하" },
-      { name: "하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
+      { name: "하하하" },
       { name: "하하하" },
     ],
   };
 
   _renderItem = ({ item, idx }) => {
     return (
-      <View style={{ width: '90%', flexDirection: 'row', alignItems: 'center', height: 65, justifyContent: 'space-between', borderBottomWidth: 1, borderColor: '#d1d1d1' }}>
-        <View style={{ flexDirection: 'row', alignItems: 'center', width: '100%' }}>
+      <SafeAreaView>
+        <View style={{ width: '90%', flexDirection: 'row', alignItems: 'center', height: 65, justifyContent: 'space-between', borderBottomWidth: 1, borderColor: '#d1d1d1' }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', width: '100%' }}>
 
-          <View style={{ width: 44, height: 44, borderRadius: 22, backgroundColor: '#ff00ff' }}></View>
+            <View style={{ width: 44, height: 44, borderRadius: 22, backgroundColor: '#ff00ff' }}></View>
 
-          <View style={{ marginLeft: 5 }}>
-            <Text style={{ fontSize: 13, color: '#000000' }}>{item.name}</Text>
+            <View style={{ marginLeft: 5 }}>
+              <Text style={{ fontSize: 13, color: '#000000' }}>{item.name}</Text>
+            </View>
           </View>
         </View>
-      </View>
+      </SafeAreaView>
     );
   }
 
